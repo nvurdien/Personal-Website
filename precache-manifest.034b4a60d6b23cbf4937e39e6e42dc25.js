a288ad424357ba3094c0c752a77665443d8e3e1c
Navie Vurdien <nvurdien@gmail.com>
2020-06-28 20:03:06 -0700

    Updated personal site

---
+++ precache-manifest.034b4a60d6b23cbf4937e39e6e42dc25.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c983142272c7459632dc0758cd85a865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f67ab05dc17fb7c1a395",
+    "url": "/static/css/3.8c4e8b5e.chunk.css"
+  },
+  {
+    "revision": "3ec08798e94dec414f88",
+    "url": "/static/css/4.9504da60.chunk.css"
+  },
+  {
+    "revision": "3a979635fe89b8ff2df8",
+    "url": "/static/css/5.2805bf3f.chunk.css"
+  },
+  {
+    "revision": "db551655cdff394cda10",
+    "url": "/static/css/main.fe6a4fbf.chunk.css"
+  },
+  {
+    "revision": "3b83d7e5050db47b9e7c",
+    "url": "/static/js/0.107c5cc5.chunk.js"
+  },
+  {
+    "revision": "f67ab05dc17fb7c1a395",
+    "url": "/static/js/3.39ba382c.chunk.js"
+  },
+  {
+    "revision": "c3f40239bcd0a13c83dfe66bd9a54414",
+    "url": "/static/js/3.39ba382c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ec08798e94dec414f88",
+    "url": "/static/js/4.a86aabd1.chunk.js"
+  },
+  {
+    "revision": "3a979635fe89b8ff2df8",
+    "url": "/static/js/5.6d4db4b0.chunk.js"
+  },
+  {
+    "revision": "a568b9128d0304f1e861",
+    "url": "/static/js/6.0f6be297.chunk.js"
+  },
+  {
+    "revision": "db551655cdff394cda10",
+    "url": "/static/js/main.247965ec.chunk.js"
+  },
+  {
+    "revision": "e5d881b0c0e5f1589287",
+    "url": "/static/js/runtime-main.a134e6e9.js"
+  },
+  {
+    "revision": "912f76c7cfb31abe71d7315f9083fd4f",
+    "url": "/static/media/acm.912f76c7.png"
+  },
+  {
+    "revision": "9d7882462d553d1e21476cb47dca3db1",
+    "url": "/static/media/cppcamp.9d788246.png"
+  },
+  {
+    "revision": "9ecc196b656265d1d9e1a6f0b1429b0e",
+    "url": "/static/media/ecs_summit.9ecc196b.png"
+  },
+  {
+    "revision": "b8a48b9447240bea42ac54c1dc59d66c",
+    "url": "/static/media/giffrey.b8a48b94.png"
+  },
+  {
+    "revision": "1f42089d79aebd1cd1cbd607259cfe1a",
+    "url": "/static/media/rabbit_net.1f42089d.png"
+  },
+  {
+    "revision": "c1202e0835766f746ad8386453a0598a",
+    "url": "/static/media/university-database-2.c1202e08.png"
+  },
+  {
+    "revision": "844e68d6c125562c6cbc35a71bbfe21b",
+    "url": "/static/media/university-database-3.844e68d6.png"
+  },
+  {
+    "revision": "d44a308110cffc5dd8f098331a449c41",
+    "url": "/static/media/university_database.d44a3081.png"
+  },
+  {
+    "revision": "e5bbda22e7e2f6eda6538fa550a38d27",
+    "url": "/static/media/webdevworkshop.e5bbda22.png"
+  }
+]);
